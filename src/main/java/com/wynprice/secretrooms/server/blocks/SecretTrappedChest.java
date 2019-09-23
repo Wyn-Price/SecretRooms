83f4b3b0b8dce2d3a26f0a760b4b0786f48b2859
Wyn-Price <wynprice999@gmail.com>
2019-09-23 16:35:58 +0100

    Fixed trapped chest power issues and visual glitches

--- src/main/java/com/wynprice/secretrooms/server/blocks/SecretTrappedChest.java
+++ src/main/java/com/wynprice/secretrooms/server/blocks/SecretTrappedChest.java
@@ -2,15 +2,12 @@ package com.wynprice.secretrooms.server.blocks;
 
 import com.wynprice.secretrooms.server.tileentity.SecretChestTileEntity;
 import net.minecraft.block.BlockState;
-import net.minecraft.tileentity.ChestTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReader;
-
-import javax.annotation.Nullable;
 
 public class SecretTrappedChest extends SecretChest {
 
@@ -27,14 +24,14 @@ public class SecretTrappedChest extends SecretChest {
     public int getWeakPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
         TileEntity tileEntity = blockAccess.getTileEntity(pos);
         if(tileEntity instanceof SecretChestTileEntity) {
-            return MathHelper.clamp(((SecretChestTileEntity) tileEntity).getNumPlayersUsing() + 3, 0, 15);
+            return MathHelper.clamp(((SecretChestTileEntity) tileEntity).getNumPlayersUsing(), 0, 15);
         }
         return 0;
     }
 
     @Override
     public boolean shouldCheckWeakPower(BlockState state, IWorldReader world, BlockPos pos, Direction side) {
-        return true;
+        return false;
     }
 
     @Override
