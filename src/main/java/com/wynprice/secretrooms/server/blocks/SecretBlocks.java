966ae7e210f460dceaa7b181375fa118414f5144
Romaindu35 <romain.sai-tanguy@sfr.fr>
2021-03-09 19:05:10 +0100

    Fix hardness and resistance for all block

--- src/main/java/com/wynprice/secretrooms/server/blocks/SecretBlocks.java
+++ src/main/java/com/wynprice/secretrooms/server/blocks/SecretBlocks.java
@@ -2,6 +2,8 @@ package com.wynprice.secretrooms.server.blocks;
 
 import com.wynprice.secretrooms.SecretRooms6;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.entity.player.PlayerEntity;
@@ -16,28 +18,28 @@ public class SecretBlocks {
 
     public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, SecretRooms6.MODID);
 
-    public static final Supplier<Block> GHOST_BLOCK = REGISTRY.register("ghost_block", () -> new GhostBlock(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
-    public static final Supplier<Block> SECRET_STAIRS = REGISTRY.register("secret_stairs", () -> new SecretStairs(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
-    public static final Supplier<Block> SECRET_LEVER = REGISTRY.register("secret_lever", () -> new SecretLever(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
+    public static final Supplier<Block> GHOST_BLOCK = REGISTRY.register("ghost_block", () -> new GhostBlock(Block.Properties.create(Materials.SRM_MATERIAL).hardnessAndResistance(2.0F).notSolid()));
+    public static final Supplier<Block> SECRET_STAIRS = REGISTRY.register("secret_stairs", () -> new SecretStairs(Block.Properties.create(Materials.SRM_MATERIAL).setRequiresTool().hardnessAndResistance(2.0F, 6.0F).notSolid()));
+    public static final Supplier<Block> SECRET_LEVER = REGISTRY.register("secret_lever", () -> new SecretLever(Block.Properties.create(Materials.SRM_MATERIAL).doesNotBlockMovement().hardnessAndResistance(0.5F).sound(SoundType.WOOD).notSolid()));
     public static final Supplier<Block> SECRET_REDSTONE = REGISTRY.register("secret_redstone", () -> new SecretRedstone(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
     public static final Supplier<Block> ONE_WAY_GLASS = REGISTRY.register("one_way_glass", () -> new OneWayGlass(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
-    public static final Supplier<Block> SECRET_WOODEN_BUTTON = REGISTRY.register("secret_wooden_button", () -> new SecretButton(Block.Properties.create(Materials.SRM_MATERIAL).notSolid(), true));
-    public static final Supplier<Block> SECRET_STONE_BUTTON = REGISTRY.register("secret_stone_button", () -> new SecretButton(Block.Properties.create(Materials.SRM_MATERIAL).notSolid(), false));
-    public static final Supplier<Block> TORCH_LEVER = REGISTRY.register("torch_lever", () -> new TorchLever(Block.Properties.create(Material.WOOD).setLightLevel((blockstate) -> 14).notSolid()));
-    public static final Supplier<Block> WALL_TORCH_LEVER = REGISTRY.register("wall_torch_lever", () -> new WallTorchLever(Block.Properties.create(Material.WOOD).setLightLevel((blockstate) -> 14).notSolid()));
-    public static final Supplier<Block> SECRET_PRESSURE_PLATE = REGISTRY.register("secret_pressure_plate", () -> new SecretPressurePlate(Block.Properties.create(Materials.SRM_MATERIAL).notSolid(), entity -> true));
-    public static final Supplier<Block> SECRET_PLAYER_PRESSURE_PLATE = REGISTRY.register("secret_player_pressure_plate", () -> new SecretPressurePlate(Block.Properties.create(Materials.SRM_MATERIAL).notSolid(), entity -> entity instanceof PlayerEntity));
-    public static final Supplier<Block> SECRET_DOOR = REGISTRY.register("secret_door", () -> new SecretDoor(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
-    public static final Supplier<Block> SECRET_IRON_DOOR = REGISTRY.register("secret_iron_door", () -> new SecretDoor(Block.Properties.create(Materials.SRM_MATERIAL_IRON).notSolid()));
-    public static final Supplier<Block> SECRET_CHEST = REGISTRY.register("secret_chest", () -> new SecretChest(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
-    public static final Supplier<Block> SECRET_TRAPDOOR = REGISTRY.register("secret_trapdoor", () -> new SecretTrapdoor(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
-    public static final Supplier<Block> SECRET_IRON_TRAPDOOR = REGISTRY.register("secret_iron_trapdoor", () -> new SecretTrapdoor(Block.Properties.create(Materials.SRM_MATERIAL_IRON).notSolid()));
-    public static final Supplier<Block> SECRET_TRAPPED_CHEST = REGISTRY.register("secret_trapped_chest", () -> new SecretTrappedChest(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
-    public static final Supplier<Block> SECRET_GATE = REGISTRY.register("secret_gate", () -> new SecretGateBlock(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
-    public static final Supplier<Block> SECRET_DUMMY_BLOCK = REGISTRY.register("secret_dummy_block", () -> new SecretBaseBlock(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
-    public static final Supplier<Block> SECRET_DAYLIGHT_DETECTOR = REGISTRY.register("secret_daylight_detector", () -> new SecretDaylightDetector(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
-    public static final Supplier<Block> SECRET_OBSERVER = REGISTRY.register("secret_observer", () -> new SecretObserver(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
-    public static final Supplier<Block> SECRET_CLAMBER = REGISTRY.register("secret_clamber", () -> new SecretClamber(Block.Properties.create(Materials.SRM_MATERIAL).notSolid()));
+    public static final Supplier<Block> SECRET_WOODEN_BUTTON = REGISTRY.register("secret_wooden_button", () -> new SecretButton(Block.Properties.create(Materials.SRM_MATERIAL).doesNotBlockMovement().hardnessAndResistance(0.5F).sound(SoundType.WOOD).notSolid(), true));
+    public static final Supplier<Block> SECRET_STONE_BUTTON = REGISTRY.register("secret_stone_button", () -> new SecretButton(Block.Properties.create(Materials.SRM_MATERIAL).doesNotBlockMovement().hardnessAndResistance(0.5F).notSolid(), false));
+    public static final Supplier<Block> TORCH_LEVER = REGISTRY.register("torch_lever", () -> new TorchLever(Block.Properties.create(Material.WOOD).setLightLevel((blockstate) -> 14).doesNotBlockMovement().hardnessAndResistance(0.5F).notSolid()));
+    public static final Supplier<Block> WALL_TORCH_LEVER = REGISTRY.register("wall_torch_lever", () -> new WallTorchLever(Block.Properties.create(Material.WOOD).setLightLevel((blockstate) -> 14).doesNotBlockMovement().hardnessAndResistance(0.5F).notSolid()));
+    public static final Supplier<Block> SECRET_PRESSURE_PLATE = REGISTRY.register("secret_pressure_plate", () -> new SecretPressurePlate(Block.Properties.create(Materials.SRM_MATERIAL).setRequiresTool().doesNotBlockMovement().hardnessAndResistance(0.5F).notSolid(), entity -> true));
+    public static final Supplier<Block> SECRET_PLAYER_PRESSURE_PLATE = REGISTRY.register("secret_player_pressure_plate", () -> new SecretPressurePlate(Block.Properties.create(Materials.SRM_MATERIAL).setRequiresTool().doesNotBlockMovement().hardnessAndResistance(0.5F).notSolid(), entity -> entity instanceof PlayerEntity));
+    public static final Supplier<Block> SECRET_DOOR = REGISTRY.register("secret_door", () -> new SecretDoor(Block.Properties.create(Materials.SRM_MATERIAL).hardnessAndResistance(3.0F)));
+    public static final Supplier<Block> SECRET_IRON_DOOR = REGISTRY.register("secret_iron_door", () -> new SecretDoor(Block.Properties.create(Materials.SRM_MATERIAL_IRON).setRequiresTool().hardnessAndResistance(5.0F).sound(SoundType.METAL).notSolid()));
+    public static final Supplier<Block> SECRET_CHEST = REGISTRY.register("secret_chest", () -> new SecretChest(Block.Properties.create(Materials.SRM_MATERIAL).hardnessAndResistance(2.5F).sound(SoundType.WOOD).notSolid()));
+    public static final Supplier<Block> SECRET_TRAPDOOR = REGISTRY.register("secret_trapdoor", () -> new SecretTrapdoor(Block.Properties.create(Materials.SRM_MATERIAL).hardnessAndResistance(2.5F).sound(SoundType.WOOD).notSolid()));
+    public static final Supplier<Block> SECRET_IRON_TRAPDOOR = REGISTRY.register("secret_iron_trapdoor", () -> new SecretTrapdoor(Block.Properties.create(Materials.SRM_MATERIAL_IRON).setRequiresTool().hardnessAndResistance(5.0F).sound(SoundType.METAL).notSolid()));
+    public static final Supplier<Block> SECRET_TRAPPED_CHEST = REGISTRY.register("secret_trapped_chest", () -> new SecretTrappedChest(Block.Properties.create(Materials.SRM_MATERIAL).hardnessAndResistance(2.5F).sound(SoundType.WOOD).notSolid()));
+    public static final Supplier<Block> SECRET_GATE = REGISTRY.register("secret_gate", () -> new SecretGateBlock(Block.Properties.create(Materials.SRM_MATERIAL).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD).notSolid()));
+    public static final Supplier<Block> SECRET_DUMMY_BLOCK = REGISTRY.register("secret_dummy_block", () -> new SecretBaseBlock(Block.Properties.create(Materials.SRM_MATERIAL).hardnessAndResistance(2.0F).notSolid()));
+    public static final Supplier<Block> SECRET_DAYLIGHT_DETECTOR = REGISTRY.register("secret_daylight_detector", () -> new SecretDaylightDetector(Block.Properties.create(Materials.SRM_MATERIAL).hardnessAndResistance(0.2F).sound(SoundType.WOOD).notSolid()));
+    public static final Supplier<Block> SECRET_OBSERVER = REGISTRY.register("secret_observer", () -> new SecretObserver(Block.Properties.create(Materials.SRM_MATERIAL).hardnessAndResistance(3.0F).setRequiresTool().notSolid()));
+    public static final Supplier<Block> SECRET_CLAMBER = REGISTRY.register("secret_clamber", () -> new SecretClamber(Block.Properties.create(Materials.SRM_MATERIAL).hardnessAndResistance(2.0F).notSolid()));
 
     @SubscribeEvent
     public static void onBlockRegistry(RegistryEvent.Register<Block> event) {
