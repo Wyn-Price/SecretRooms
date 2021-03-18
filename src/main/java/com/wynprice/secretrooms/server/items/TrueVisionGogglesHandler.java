00afc01d4f802000638f22e2a2cbbbf7d43eab67
Wyn Price <wynprice999@gmail.com>
2021-03-18 06:34:58 +0000

    Better handling when goggles are put on

--- src/main/java/com/wynprice/secretrooms/server/items/TrueVisionGogglesHandler.java
+++ src/main/java/com/wynprice/secretrooms/server/items/TrueVisionGogglesHandler.java
@@ -1,9 +1,13 @@
 package com.wynprice.secretrooms.server.items;
 
+import com.wynprice.secretrooms.server.tileentity.SecretTileEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -24,7 +28,13 @@ public class TrueVisionGogglesHandler {
         boolean wearing = isWearingGoggles(player);
         if(wearing != clientWearingItem) {
             clientWearingItem = wearing;
-            Minecraft.getInstance().worldRenderer.loadRenderers();
+            WorldRenderer renderer = Minecraft.getInstance().worldRenderer;
+            for (TileEntity tileEntity : player.world.loadedTileEntityList) {
+                if(tileEntity instanceof SecretTileEntity) {
+                    BlockPos pos = tileEntity.getPos();
+                    renderer.notifyBlockUpdate(null, pos, null, null, 8);
+                }
+            }
         }
     }
 
