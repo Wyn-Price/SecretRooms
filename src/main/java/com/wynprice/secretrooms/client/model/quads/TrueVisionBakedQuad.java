5258e56fb83140cbd3b1291478b598853ea14444
Wyn Price <wynprice999@gmail.com>
2021-03-19 03:35:12 +0000

    Removed old code from true vision baked quads

--- src/main/java/com/wynprice/secretrooms/client/model/quads/TrueVisionBakedQuad.java
+++ src/main/java/com/wynprice/secretrooms/client/model/quads/TrueVisionBakedQuad.java
@@ -38,81 +38,33 @@ public class TrueVisionBakedQuad {
 
     public static BakedQuad generateQuad(BakedQuad quad) {
         int[] data = Arrays.copyOf(quad.getVertexData(), quad.getVertexData().length);
-        List<Vector3f> positions = new ArrayList<>();
-        List<Vector2f> uvs = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
             int j = DefaultVertexFormats.BLOCK.getIntegerSize() * i;
-            positions.add(new Vector3f(
-                intBitsToFloat(data[j]) + 0.001F*quad.getFace().getXOffset(),
-                intBitsToFloat(data[j+1]) + 0.001F*quad.getFace().getYOffset(),
-                intBitsToFloat(data[j+2]) + 0.001F*quad.getFace().getZOffset()
-            ));
-            uvs.add(new Vector2f(
-                intBitsToFloat(data[j+4]),
-                intBitsToFloat(data[j+5])
-            ));
-        }
 
-        positions.sort((o1, o2) -> {
-            if (!DoubleMath.fuzzyEquals(o1.getX(), o2.getX(), 0.001D)) {
-                return Double.compare(o1.getX(), o2.getX());
-            }
-            if (!DoubleMath.fuzzyEquals(o1.getY(), o2.getY(), 0.001D)) {
-                return Double.compare(o2.getY(), o1.getY());
-            }
-            if (!DoubleMath.fuzzyEquals(o1.getZ(), o2.getZ(), 0.001D)) {
-                return Double.compare(o1.getZ(), o2.getZ());
-            }
-            return 0;
-        });
-        uvs.sort((o1, o2) -> {
-            if (!DoubleMath.fuzzyEquals(o1.x, o2.x, 0.001D)) {
-                return Double.compare(o1.x, o2.x);
-            }
-            if (!DoubleMath.fuzzyEquals(o1.y, o2.y, 0.001D)) {
-                return Double.compare(o1.y, o2.y);
-            }
-            return 0;
-        });
+            float x = intBitsToFloat(data[j]) + 0.001F*quad.getFace().getXOffset();
+            float y = intBitsToFloat(data[j+1]) + 0.001F*quad.getFace().getYOffset();
+            float z = intBitsToFloat(data[j+2]) + 0.001F*quad.getFace().getZOffset();
 
-        Vector3f tempPos = positions.get(3);
-        positions.set(3, positions.get(2));
-        positions.set(2, tempPos);
-
-        Vector2f tempUv = uvs.get(3);
-        uvs.set(3, uvs.get(2));
-        uvs.set(2, tempUv);
-
-        if(quad.getFace().getAxisDirection() == Direction.AxisDirection.NEGATIVE) {
-            Collections.reverse(positions);
-            Collections.reverse(uvs);
-        }
-
-        for (int i = 0; i < 4; i++) {
-            int j = DefaultVertexFormats.BLOCK.getIntegerSize() * i;
-            Vector3f pos = positions.get(i);
-            Vector2f uv = uvs.get(i);
-
-            data[j] = floatToRawIntBits(pos.getX());
-            data[j+1] = floatToRawIntBits(pos.getY());
-            data[j+2] = floatToRawIntBits(pos.getZ());
+            data[j] = floatToRawIntBits(x);
+            data[j+1] = floatToRawIntBits(y);
+            data[j+2] = floatToRawIntBits(z);
 
             float ui;
             float vi;
 
             switch (quad.getFace().getAxis()) {
                 case X:
-                    ui = pos.getZ();
-                    vi = 1-pos.getY();
+                    ui = z;
+                    vi = 1-y;
                     break;
                 case Y:
                 default:
-                    ui = pos.getX();
-                    vi = pos.getZ();
+                    ui = x;
+                    vi = z;
                     break;
                 case Z:
-                    ui = pos.getX();
-                    vi = 1-pos.getY();
+                    ui = x;
+                    vi = 1-y;
                     break;
             }
 
