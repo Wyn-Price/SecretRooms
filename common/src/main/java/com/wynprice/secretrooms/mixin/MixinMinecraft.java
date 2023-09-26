50c2f476033df8173efd1e2e71686e200fbdf590
Wyn Price <github@wynprice.com>
2023-09-26 14:38:31 +0100

    Try get mixins to work?

---
+++ common/src/main/java/com/wynprice/secretrooms/mixin/MixinMinecraft.java
@@ -0,0 +1,8 @@
+package com.wynprice.secretrooms.mixin;
+
+import net.minecraft.world.entity.vehicle.Minecart;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Minecart.class)
+public class MixinMinecraft {
+}
